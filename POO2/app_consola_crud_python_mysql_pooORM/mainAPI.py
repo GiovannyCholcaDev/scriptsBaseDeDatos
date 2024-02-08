bd8fbf5f259a6e2e4e564189443f199ba5078145
Giovanny Cholca <byroncholca@gmail.com>
2024-02-08 01:09:16 -0500

    fastAPI con entorno virtual

--- POO2/app_consola_crud_python_mysql_pooORM/mainAPI.py
+++ POO2/app_consola_crud_python_mysql_pooORM/mainAPI.py
@@ -2,8 +2,16 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+app.title = "APIs documentacion con fastAPI"
+app.version = "1.0.0"
+
+"""
 @app.get('/')
 def message():
-    return 'hola mundo web'
+    return 'hola mundo web con python y fastAPI'
+"""
 
 
+@app.get('/', tags=['home'])
+def message():
+    return 'hola mundo web con python y fastAPI'
