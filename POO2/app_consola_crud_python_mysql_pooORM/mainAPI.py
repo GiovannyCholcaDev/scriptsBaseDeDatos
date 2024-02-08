ea5468b6165829525551ee7e390be2024bdc8a73
CPN\gcholca <giovanny.cholca@cpn.fin.ec>
2024-02-08 17:53:55 -0500

    apis get

--- POO2/app_consola_crud_python_mysql_pooORM/mainAPI.py
+++ POO2/app_consola_crud_python_mysql_pooORM/mainAPI.py
@@ -5,6 +5,25 @@ app = FastAPI()
 app.title = "APIs documentacion con fastAPI"
 app.version = "1.0.0"
 
+movies = [
+    {
+    "id":1,
+    "title":"Avengers 1",
+    "overview": "Primer encuentro de los vengadores",
+    "year": 2020,
+    "rating": 7.8,
+    "category": "Accion"
+    },
+    {
+    "id":2,
+    "title":"Avengers 2",
+    "overview": "vengadores la era de ultron",
+    "year": 2015,
+    "rating": 9.8,
+    "category": "Accion"
+    }
+]
+
 """
 @app.get('/')
 def message():
@@ -15,3 +34,22 @@ def message():
 @app.get('/', tags=['home'])
 def message():
     return 'hola mundo web con python y fastAPI'
+
+
+
+@app.post('/apis', tags=['client'])
+def message():
+    return {"mensaje":"hola mundo web con python y fastAPI post", "CODE":"0", "status": "true"}
+
+
+@app.get('/movies', tags=['movies'])
+def getmovies():
+    return movies
+
+
+@app.get('/movies/{id}', tags=['movies'])
+def getmovies(id:int):
+    for item in movies:
+        if item["id"] == id:
+            return item
+
